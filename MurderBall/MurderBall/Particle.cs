1f4398726bb68356fae326e19f83cdb323895174
Francisco Canas <mailfrancisco@gmail.com>
2011-12-30 13:30:30 -0500

    Added particle classes

---
+++ MurderBall/MurderBall/Particle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace MurderBall
+{
+    public class Particle
+    {
+        public Texture2D texture { get; set; }
+        public Vector2 position { get; set; }
+        public Vector2 velocity { get; set; }
+        public float angle { get; set; }
+        public float angularVelocity { get; set; }
+        public Color color {get; set;}
+        public float size { get; set; }
+        public int TTL { get; set; } // time to live
+
+        /// <summary>
+        /// Constructor
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="pos"></param>
+        /// <param name="vel"></param>
+        /// <param name="angle"></param>
+        /// <param name="angVel"></param>
+        /// <param name="col"></param>
+        /// <param name="size"></param>
+        /// <param name="ttl"></param>
+        public Particle(Texture2D text, Vector2 pos, Vector2 vel,
+            float angle, float angVel, Color col, float size, int ttl)
+        {
+            this.texture = text;
+            this.position = pos;
+            this.velocity = vel;
+            this.angle = angle;
+            this.angularVelocity = angVel;
+            this.color = col;
+            this.size = size;
+            this.TTL = ttl;
+
+        }
+
+        /// <summary>
+        /// Update runs every frame.
+        /// </summary>
+        public void Update() 
+        {
+            TTL--;
+            position += velocity;
+            angle += angularVelocity;
+        }
+
+        /// <summary>
+        /// Draw runs every time we draw to screen.
+        /// 
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle sourceRect = new Rectangle(0, 0, texture.Width, texture.Height);
+            Vector2 origin = new Vector2(texture.Width / 2, texture.Height / 2);
+
+            spriteBatch.Draw(texture, position, sourceRect, color, angle,
+                origin, size, SpriteEffects.None, 0f);
+        }
+
+    }
+}
---
+++ MurderBall/MurderBall/ParticleEngine.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace MurderBall
+{
+    public class ParticleEngine
+    {
+
+        private Random random;
+        public Vector2 emitterLocation { get; set; }
+        private List<Particle> particles;
+        private List<Texture2D> textures;
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="textures"></param>
+        /// <param name="location"></param>
+        public ParticleEngine(List<Texture2D> textures, Vector2 location)
+        {
+            emitterLocation = location;
+            this.textures = textures;
+            this.particles = new List<Particle>();
+            random = new Random();
+        }
+
+        /// <summary>
+        /// Creates a new random particle
+        /// </summary>
+        /// <returns></returns>
+        private Particle GenerateNewParticle()
+        {
+            Texture2D texture = textures[random.Next(textures.Count)];
+            Vector2 pos = emitterLocation;
+            Vector2 velocity = new Vector2(
+                1f * (float) (random.NextDouble() * 2 - 1),
+                1f * (float) (random.NextDouble() * 2 - 1));
+
+            float angle = 0;
+            float angularVelocity = 0.1f * (float)(random.NextDouble() * 2 - 1);
+            Color color = new Color(
+                (float)random.NextDouble(),
+                (float)random.NextDouble(),
+                (float)random.NextDouble());
+
+            float size = (float)random.NextDouble();
+            int ttl = 20 + random.Next(40);
+
+
+            return new Particle(texture, 
+                pos,
+                velocity,
+                angle,
+                angularVelocity,
+                color,
+                size,
+                ttl);
+        }
+
+        /// <summary>
+        /// Updates all particles
+        /// </summary>
+        public void Update()
+        {
+            int total = 10;
+
+            // Add some new particles
+            for (int i = 0; i < total; i++)
+            {
+                particles.Add(GenerateNewParticle());
+
+            }
+
+            // Update and remove old particles
+            for (int curP = 0; curP < particles.Count; curP++)
+            {
+                // Update
+                particles[curP].Update();
+
+                // Remove
+                if (particles[curP].TTL <= 0)
+                {
+                    particles.RemoveAt(curP);
+                    curP--;
+                }
+            }
+        } // End update
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // spriteBatch.Begin();
+            foreach (Particle p in particles) {
+                p.Draw(spriteBatch);
+            }
+        }
+    
+    }
+}
